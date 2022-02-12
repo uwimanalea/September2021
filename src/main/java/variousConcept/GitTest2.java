ab41713303e0ff67ecdd41e080cdf901bc2dc879
Owner@eclipse-seleniumspt2021 <leauwimana8@gmail.com>
2022-02-11 17:59:49 -0800

    3rd commit

---
+++ src/main/java/variousConcept/GitTest2.java
@@ -0,0 +1,13 @@
+package variousConcept;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git test 2");
+	}
+	public static void printme() {
+		System.out.println("get test 1");
+	}
+}
